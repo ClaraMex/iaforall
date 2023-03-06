8ea96bbd85139ece6140903e8dc111060d0e0435
patou <wampepatrick@gmail.com>
2023-03-06 16:00:40 +0100

    maj3

--- ia4all/authentification/views.py
+++ ia4all/authentification/views.py
@@ -6,7 +6,27 @@ def inscription(request):
     return render(request, "inscription.html")
 
 def connexion(request):
-    return render(request, "connexion.html")
+    # La méthode POSt est utilisé quand des infos
+    # sont envoyées au back-end
+    # Autrement dit, on a appuyé sur le bouton
+    # submit
+    message = ""
+    if request.method == "POST":
+        identifiant = request.POST["identifiant"]
+        motdepasse = request.POST["motdepasse"]
+        utilisateur = authenticate(username = identifiant,
+                                   password = motdepasse)
+        if utilisateur is not None:
+            login(request, utilisateur)
+            return render(request, "index.html")
+        else:
+            message = "Identifiant ou mot de passe incorrect"
+            return render(request, "connexion.html", {"message": message})
+    # Notre else signifie qu'on vient d'arriver
+    # sur la page, on a pas encore appuyé sur le
+    # bouton submit
+    else:
+        return render(request, "connexion.html")
 
 def deconnexion(request):
     logout(request)
